b2efe835bdbda8722008d2263113ec9f4d957a52
Viraj Kothari <viraj@softhealer.com>
2024-07-11 15:07:07 +0530

    [UPDATE] sh_sale_report

---
+++ sh_sale_report/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Sale Report",
+    "author": "Softhealer Technologies",
+    "website": "https://www.softhealer.com",
+    "category": "Extra Tools",
+    "license": "OPL-1",
+    "support": "support@softhealer.com",
+    "summary": "",
+    "description": """""",
+    "version": "16.0.1",
+    "depends": ["sale_management"],
+    "application": True,
+    "data": [
+            "views/sale_order_views.xml",
+            "views/account_move_line_views.xml",
+
+            "views/menus_sale.xml",
+
+    ],
+    
+    "auto_install": False,
+    "installable": True,
+
+}
